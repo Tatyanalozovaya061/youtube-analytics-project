6b570481b3388c664fd1eb5434a0c8eeb0291272
tatyana lozovaya <tatka8632@gmail.com>
2023-10-09 21:48:09 +0300

    реализация метода print_info

--- src/channel.py
+++ src/channel.py
@@ -1,7 +1,15 @@
+import json
+import os
 
+from googleapiclient.discovery import build
+
+# api_key: str = os.getenv('API_KEY')
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key = 'AIzaSyCMtkUSrjlw7gCh17viJ8v14Q7jzZwQc28'
+    # api_key: str = os.getenv('API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
@@ -9,4 +17,5 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        print(self.channel_id)
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
