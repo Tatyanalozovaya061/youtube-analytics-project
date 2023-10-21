27d73ef610666e0a0e4255a8704309cf5c852440
tatyana lozovaya <tatka8632@gmail.com>
2023-10-21 11:26:47 +0300

    домашка 15.1

--- src/video.py
+++ src/video.py
@@ -10,17 +10,18 @@ class Video:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.video_id = video_id
         youtube = build('youtube', 'v3', developerKey=Video.api_key)
+        # video_id = 'gaoc9MPZ4bw'
         video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                id=video_id
                                                ).execute()
         # print(json.dumps(video_response, indent=2, ensure_ascii=False))
         self.title = video_response['items'][0]['snippet']['title']
-        self.url = f'https://www.youtube.com/watch?v=gaoc9MPZ4bw'
+        self.url = f'https://www.youtube.com/watch?v={video_id}'
         self.view_count = video_response['items'][0]['statistics']['viewCount']
         self.like_count = video_response['items'][0]['statistics']['likeCount']
 
     def __str__(self):
-        return str(f'{self.title} ({self.url})')
+        return f'{self.title}'
 
 class PLVideo(Video):
 
@@ -29,5 +30,5 @@ class PLVideo(Video):
         self.playlist_id = playlist_id
 
 
-# video1 = Video('AWX4JnAnjBE')  # 'AWX4JnAnjBE' - это id видео из ютуб
-# video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
+video1 = Video('AWX4JnAnjBE')  # 'AWX4JnAnjBE' - это id видео из ютуб
+video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
